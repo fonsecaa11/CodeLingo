b76d82ca54d0472ed0e64113ecf34d8fd0a59209
Yuri <yurihfrancelinocneves@gmail.com>
2024-12-03 15:35:31 +0000

    deu ruim

--- app/src/main/java/com/example/codelingo/RegisterActivity.kt
+++ app/src/main/java/com/example/codelingo/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.codelingo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,14 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Botão para navegar para MainActivity
+        val loginButton: Button = findViewById(R.id.Login2)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Fecha a RegisterActivity para que não volte ao pressionar o botão "voltar".
         }
         val auth = FirebaseAuth.getInstance()
         val db = FirebaseFirestore.getInstance()
