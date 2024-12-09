48e5d61aab1e24ee7f9052bae740c1f1295fbe3c
Yuri <yurihfrancelinocneves@gmail.com>
2024-12-09 13:59:41 +0000

    Fix Button Login

--- app/src/main/java/com/example/codelingo/LoginActivity.kt
+++ app/src/main/java/com/example/codelingo/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.codelingo
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 
-class LoginActivity<Button> : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
