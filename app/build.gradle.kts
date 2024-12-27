60245fe1e1dc7344472a41b107e41c6e1130b722
Gonçalo Fonseca <goncalofonseca2001@gmail.com>
2024-12-27 15:51:30 +0000

    sla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation (libs.firebase.firestore.ktx)
-
 }
